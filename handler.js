7826ef0ad18a5c1b0d8c8469510173f3fb32209b
awitherow <au.witherow@gmail.com>
2018-08-18 09:46:07 -0400

    finalize basic mailgun script

--- handler.js
+++ handler.js
@@ -2,14 +2,50 @@
 
 "use strict";
 
-module.exports.mail = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v1.0! Your function executed successfully!",
-      input: event
-    })
+const AWS = require("aws-sdk");
+const SES = new AWS.SES();
+
+function sendMail(formData, cb) {
+  const { contact, from, subject, body } = formData.body;
+
+  const emailParams = {
+    Source: "mailgun@awitherow.com",
+    ReplyToAddresses: [from],
+    Destination: {
+      ToAddresses: ["me@awitherow.com"] // SES RECEIVING EMAIL
+    },
+    Message: {
+      Body: {
+        Text: {
+          Charset: "UTF-8",
+          Data: `Contact: ${contact}\nEmail: ${from}\n\n ${body}`
+        }
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: `[${contact}] - ${subject}`
+      }
+    }
   };
 
-  callback(null, response);
+  SES.sendEmail(emailParams, cb);
+}
+
+module.exports.mail = (event, context, callback) => {
+  const formData = JSON.parse(event.body);
+
+  sendMail(formData, function(err, data) {
+    const response = {
+      statusCode: err ? 500 : 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "https://awitherow.com"
+      },
+      body: JSON.stringify({
+        message: err ? err.message : data
+      })
+    };
+
+    callback(null, response);
+  });
 };
